a6ac23ef2ac66a178fed4f4bfdd4c64444a2e19b
narradorww <rodrigo.anst@gmail.com>
2022-11-23 02:03:38 -0300

    Teste de manipulaçãpo de injeção d eestylos por javascript

---
+++ js/main.js
@@ -0,0 +1,20 @@
+
+let nav = document.getElementsByClassName('btn-group-vertical')[0];
+nav.style.display= "flex"
+nav.style.flexDirection= "row"
+nav.style.justifyContent= "space-between"
+nav.style.alignItems= "center"
+nav.style.width= "100%"
+nav.style.padding = "0 10px"
+
+let header = document.getElementsByClassName("jumbotron")[0];
+header.style.color = "white"
+header.style.background = "#343a40"
+header.style.display = "flex"
+header.style.flexDirection = "column"
+
+
+let button = document.getElementsByClassName("btn btn-primary btn-lg")[0];
+button.style.background = "#28a745"
+button.style.border = "none"
+
